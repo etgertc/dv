f684e55558ca50c57c529ae14dc29c88990a7776
etgertc <104611103+etgertc@users.noreply.github.com>
2022-04-28 21:16:40 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+const a = prompt("a1 ni kiriting")
+const n = prompt("n-ni kiriting")
+const d = prompt("d ni kiriting")
+
+let ber = a * 2
+let nn = n - 1
+let gg = nn * d
+let fgh = gg + ber
+let fhh = fgh * n
+let dvd = fhh / 2
+alert(dvd)
+document.write(`<p style=" font-size: 100px; color: black; font-weight: 900;">${dvd}</p>`)
